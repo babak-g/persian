0f0cd5233991d7c8b98418772c38e1d993eee94a
babak-g <143975395+babak-g@users.noreply.github.com>
2023-11-02 21:29:05 -0700

    add base website template of translator

---
+++ src/config.cjs
@@ -0,0 +1,9 @@
+export default {
+  siteBaseUrl: '/',
+  siteLanguage: "en",
+  siteTitle: "learnpersian.ai",
+  siteCopyright: "© 2023 copyright text.",
+  dateFormat: "MMMM D, YYYY",
+  memus:[
+  ],
+};
